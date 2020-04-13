90910a236c78b1d780598ed68e7bafe0e942e61c
puzzledpublic <puzzledpublic@gmail.com>
2020-04-13 22:55:50 +0900

    gatsby

---
+++ src/pages/post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import {graphql} from 'gatsby'
+import Layout from '../components/layout'
+
+export default function Post() {
+    return (
+        <Layout>
+
+        </Layout>
+    )
+}
+
